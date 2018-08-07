742cc75d2b6f1cfbedbe655315bf49a0f10f1437
segabriel <sergiy.gabriel@jivygroup.com>
2018-08-07 18:33:49 +0300

    Added javadocs

--- src/main/java/io/scalecube/app/loader/ServiceLoaderUtil.java
+++ src/main/java/io/scalecube/app/loader/ServiceLoaderUtil.java
@@ -14,24 +14,55 @@ public final class ServiceLoaderUtil {
     // Do not instantiate
   }
 
+  /**
+   * Finds the first implementation of the given service type and creates its instance.
+   *
+   * @param clazz service type
+   * @return the first implementation of the given service type
+   */
   public static <T> Optional<T> findFirst(Class<T> clazz) {
     ServiceLoader<T> load = ServiceLoader.load(clazz);
     return StreamSupport.stream(load.spliterator(), false).findFirst();
   }
 
+  /**
+   * Finds the first implementation of the given service type using the given predicate
+   * to filter out found service types and creates its instance.
+   *
+   * @param clazz service type
+   * @param predicate service type predicate
+   * @return the first implementation of the given service type
+   */
   public static <T> Optional<T> findFirst(Class<T> clazz, Predicate<? super T> predicate) {
     ServiceLoader<T> load = ServiceLoader.load(clazz);
     Stream<T> stream = StreamSupport.stream(load.spliterator(), false);
     return stream.filter(predicate).findFirst();
   }
 
+  /**
+   * Finds all implementations of the given service type and creates their instances.
+   *
+   * @param clazz service type
+   * @return implementations' set of the given service type
+   */
   public static <T> Collection<T> findAll(Class<T> clazz) {
     ServiceLoader<T> load = ServiceLoader.load(clazz);
-    return StreamSupport.stream(load.spliterator(), false).collect(Collectors.toList());
+    return StreamSupport.stream(load.spliterator(), false)
+        .collect(Collectors.toList());
   }
 
+  /**
+   * Finds all implementations of the given service type using the given predicate
+   * to filter out found service types and creates their instances.
+   *
+   * @param clazz service type
+   * @param predicate service type predicate
+   * @return implementations' set of the given service type
+   */
   public static <T> Collection<T> findAll(Class<T> clazz, Predicate<? super T> predicate) {
     ServiceLoader<T> load = ServiceLoader.load(clazz);
-    return StreamSupport.stream(load.spliterator(), false).filter(predicate).collect(Collectors.toList());
+    return StreamSupport.stream(load.spliterator(), false)
+        .filter(predicate)
+        .collect(Collectors.toList());
   }
 }
